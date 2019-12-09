cb84a06c37dc93881806e0d8d59cc5c8c3346dc9
Joseph Tatum <josephtatum@gmail.com>
2019-12-09 15:19:12 -0800

    created all endpoints and first two passing tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,4 +3,21 @@ const app = require('../lib/app');
 
 describe('createResponse', () => {
 
+  it('should return a string "hi" when a user hits the "/" endpoint', async (done) => {
+    return request(app)
+      .get('/')
+      .then(res => {
+        expect(res.text).toEqual('hi');
+        done();
+      });
+  });
+
+  it('should return <h1>red</h1> when a user hits the "red" endpoint', async (done) => {
+    return request(app)
+      .get('/red')
+      .then(res => {
+        expect(res.text).toEqual('<h1>red</h1>');
+        done();
+      });
+  });
 });
